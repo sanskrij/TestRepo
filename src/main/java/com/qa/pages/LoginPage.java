817bd05a167f04d66f6a1a9db2b91c0eece29d34
Sanskriti <sanskritij.170@gmail.com>
2019-10-28 21:22:40 +0530

    git commit feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,4 +2,13 @@ package com.qa.pages;
 
 public class LoginPage {
 
+	public void loginPage() {
+		System.out.println("Login Method");
+	}
+	
+
+	public void Set() {
+		System.out.println("Login Set");
+	}
+
 }
